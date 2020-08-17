04f42f513874c61de9062b434bbe1de021060cbf
Rafay Khan <rafayak16@gmail.com>
2020-08-17 14:09:50 +0500

    add solution to exercise 1.1

---
+++ 1.1/ex_11.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+;exercise 1.1
+
+10 ; 10
+(+ 5 3 4) ; 12
+(- 9 1) ; 8
+(/ 6 2) ; 3
+(+ (* 2 4) (- 4 6)) ; 6
+(define a 3) ; print nothing
+(define b (+ a 1)) ; print nothing
+(+ a b (* a b)) ; 19
+(= a b) ; #f
+
+(if (and (> b a) (< b (* a b)))
+    b
+    a)  ; and condition met -> 4
+
+(cond ((= a b) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25)) ; b==4 -> 16
+
+(+ 2 (if (> b a) b a)) ; 2 + 4 -> 6
+
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a  1)) ; a < b -> 4 * 4 -> 16
